8fd2f846a659747323e594eb035e370162ec0c27
secretbit007 <secretbit007@gmail.com>
2025-03-31 21:19:15 +0900

    Update admin model

--- cardealer/admin.py
+++ cardealer/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+admin.site.site_title = "Carstrucx"
+admin.site.site_header = "Carstrucx administration"
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'last_login', 'username', 'first_name', 'last_name', 'email', 'date_joined', 'is_active')
+    
+class AdAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'title', 'price', 'date_posted')
+    search_fields = ('user__username', 'id',)
+
+admin.site.register(models.CustomUser, UserAdmin)
+admin.site.register(models.Ad, AdAdmin)
