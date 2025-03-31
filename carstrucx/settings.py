33cba2b572b3b9588d98a85dbbee994f5ac70642
secretbit007 <secretbit007@gmail.com>
2025-03-31 18:19:03 +0900

    Update hosts list

--- carstrucx/settings.py
+++ carstrucx/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', 'carstrucx.com']
+ALLOWED_HOSTS = ['carstrucx.com', 'www.carstrucx.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
