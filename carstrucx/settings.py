18ba9736bccf477d4df588e68655c650046ee9e9
secretbit007 <secretbit007@gmail.com>
2025-03-31 19:03:28 +0900

    Modify static path setting

--- carstrucx/settings.py
+++ carstrucx/settings.py
@@ -127,7 +127,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = '/home/carstruc/public_html/static/'
+
+MEDIA_ROOT = '/home/carstruc/public_html/media/'
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
